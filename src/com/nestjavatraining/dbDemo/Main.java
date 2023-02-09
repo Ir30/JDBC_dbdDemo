09f3cbdd283304be1a9376c741ce9e4caeb49ab9
irshad007 <irshadmuhammedch8@gmail.com>
2023-02-09 18:23:13 +0530

    updated the code with prepare statement

--- src/com/nestjavatraining/dbDemo/Main.java
+++ src/com/nestjavatraining/dbDemo/Main.java
@@ -2,33 +2,44 @@ package com.nestjavatraining.dbDemo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Scanner;
+
+
 
 public class Main {
 	
-	static void insertEmployee(String name,Statement s) throws SQLException {
-		String query = String.format("INSERT INTO employee (employeename) VALUES('%s');", name);
-		s.executeUpdate(query);
+	static void insertEmployee(String name,Connection c) throws SQLException {
+		PreparedStatement statement = c.prepareStatement("insert into employee(employeename)values(?)");
+		statement.setString(1, name);
+		int i = statement.executeUpdate();
+		System.out.println(i+" records inserted");
 	}
-	static void searchEmployee(int id,Statement s) throws SQLException  {
-		String query = String.format("SELECT * FROM `employee` WHERE `employeeid`= %x",id );
-		ResultSet resultSet = s.executeQuery(query);
+	static void searchEmployee(int id,Connection c) throws SQLException  {
+		PreparedStatement statement = c.prepareStatement("select * from employee where employeeid=?");
+		statement.setInt(1,id);
+		ResultSet resultSet = statement.executeQuery(); 
 		while(resultSet.next()) {
 			System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2));
 		}
 	}
 	
-	static void updateEmployee(int id,String name,Statement s) throws SQLException {
-		String query = String.format("UPDATE employee SET employeename = '%s' WHERE employeeid = %x; ",name,id);
-		s.executeUpdate(query);	
+	static void updateEmployee(int id,String name,Connection c) throws SQLException {
+		PreparedStatement statement = c.prepareStatement("UPDATE employee SET employeename = ? WHERE employeeid = ?");
+		statement.setString(1, name);
+		statement.setInt(2, id);
+		int i=statement.executeUpdate();
+		System.out.println(i+" records updated");
+		
 	}
 	
-	static void deleteEmoloyee(int id,Statement s) throws SQLException {
-		String query = String.format("DELETE FROM employee WHERE employeeid = %x",id);
-		s.executeUpdate(query);
+	static void deleteEmoloyee(int id,Connection c) throws SQLException {
+		PreparedStatement statement = c.prepareStatement("DELETE FROM employee WHERE employeeid =?");
+		statement.setInt(1, id);
+		int i = statement.executeUpdate();
+		System.out.println(i+" recod deleted");
 	}
 	
 	public static void main(String[] args) {
@@ -41,7 +52,7 @@ public class Main {
 			String password = "";
 			
 			Connection connection = DriverManager.getConnection(connectionString,userName,password);
-			Statement statement = connection.createStatement();
+			
 			while(true) {
 				System.out.println("*****MENU*****\n1.Insert Employee\n2.Search Employee\n3.Update Employee\n4.Delete Employee\n5.Exit");
 				int choice = sc.nextInt();
@@ -50,13 +61,13 @@ public class Main {
 					case 1:{
 						System.out.println("Enter employee name: ");
 						String name = sc.next();
-						insertEmployee(name,statement);
+						insertEmployee(name,connection);
 						break;
 					}
 					case 2:{
 						System.out.println("Enter employee id: ");
 						int id = sc.nextInt();
-						searchEmployee(id,statement);
+						searchEmployee(id,connection);
 						break;
 					}
 					case 3:{
@@ -64,18 +75,21 @@ public class Main {
 						int id = sc.nextInt();
 						System.out.println("Enter employee name to update: ");
 						String name = sc.next();
-						updateEmployee(id,name,statement);
+						updateEmployee(id,name,connection);
 						break;
 					}
 					case 4:{
 						System.out.println("Enter employee id: ");
 						int id = sc.nextInt();
-						deleteEmoloyee(id,statement);
+						deleteEmoloyee(id,connection);
 						break;
 					}
 					case 5:{
 						connection.close();
 						System.exit(0);
+					}
+					default:{
+						System.out.println("Enter a valid choice");
 					}
 				}
 				
